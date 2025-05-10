33e95a14b03b259ada0c527cc34eb5a5de49455d
nadirboudia <n_boudia@estin.dz>
2025-05-11 00:52:22 +0100

    up

--- src/pages/Productdetails.jsx
+++ src/pages/Productdetails.jsx
@@ -1,5 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router'
+import { FaRegStarHalfStroke } from "react-icons/fa6";
+import { FaStar } from "react-icons/fa6";
+import './productdetails.css'
+import { TiShoppingCart } from "react-icons/ti";
+import { CiHeart } from "react-icons/ci";
+import { FaShare } from "react-icons/fa";
 
 function Productdetails() {
     const {id} =useParams()
@@ -31,8 +37,46 @@ if(!product) return <p>Product Not found </p>
 
   return (
 
-    <div>
-      jj
+    <div className='itemdetails'>
+     <div className="container">
+           <div className="imagesitem">
+        <div className="bigimage">
+        <img  id="big_img" src={product.images[0]} alt={product.title} />
+        </div>
+        <div className="smalimage">
+           {product.images.map((t , index)=>{
+            return <img src={t} key={index} alt={product.title} onClick={()=>{
+              document.getElementById("big_img").src=t 
+            }}></img>
+           })}
+        </div>
+            </div>
+
+
+
+         <div className="detailsitem">
+              <h1 className='name'>{product.title}</h1>
+              <div className="stars">
+                  <FaStar />
+                  <FaStar />
+                  <FaStar />
+                  <FaStar />
+                  <FaRegStarHalfStroke />
+                  </div>
+               <p className='price'>{product.price}$</p>
+               <h5> Availability :  <span>{product.availabilityStatus}</span> </h5>
+               <h5> Brand :  <span>{product.brand}</span> </h5>
+              <p className='para'>{product.description}</p>
+               <h5 className='stock'> <span> Hurry Up ! Only {product.stock}  Products left in stock . </span></h5>
+
+              <button className='btn'>Add to cart <TiShoppingCart /></button>
+              <div className='icon'>
+                    <CiHeart />
+                    <FaShare />
+               </div>
+        </div>
+
+     </div>
     </div>
   )
 }
