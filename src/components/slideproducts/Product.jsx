b6fba57b1f2e0322e6d50210e6c54d42563589a1
nadirboudia <n_boudia@estin.dz>
2025-05-24 00:12:48 +0100

    Initial commit

--- src/components/slideproducts/Product.jsx
+++ src/components/slideproducts/Product.jsx
@@ -1,20 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FaRegStarHalfStroke } from "react-icons/fa6";
 import { FaStar } from "react-icons/fa6";
 import { FaCartArrowDown } from "react-icons/fa6";
 import { CiHeart } from "react-icons/ci";
 import { FaShare } from "react-icons/fa";
-import { Link } from 'react-router';
+import { Link, useNavigate } from 'react-router';
 import "./slidproduct.css"
 import { useContext } from 'react';
 import { CardContext } from '../context/Cardcontext';
 import { FaCheck } from "react-icons/fa"; 
 import toast from 'react-hot-toast';
 
+
 function product({New}) {
+const navigate = useNavigate()
 
 const { addToCart ,cartItems }= useContext(CardContext);
-
+const [clicked, setClicked] = useState(false);
 
 const playSound = () => {
     const audio = new Audio('/notification.mp3'); // تأكد من وضع الملف في public
@@ -26,24 +28,47 @@ const playSound = () => {
     
   };
 
-  function HandleAddtoCart(){
-    addToCart(New)
- handleClick ()
-    toast.success(
+   const HandleAddtoCart = () => {
+    addToCart(New);
+
+  if (clicked) 
+    {return null;}
+  else{
+      toast.success(
       <div className='toastcontent'>
-       <div className="toastimg">
-         <img src={New.images[0]}  />
-       </div>
-       <div className="toastdetails">
-        <h3>{New.title}</h3>
-        <h5>Added To cart </h5>
-        <button className='btn'>View Cart</button>
-       </div>
-      </div>
-      
-,{duration : 3500}
-    )
+        <div className="toastimg">
+          <img src={New.images[0]} />
+        </div>
+        <div className="toastdetails">
+          <h3>{New.title}</h3>
+          <h5>Added To Cart</h5>
+          <button
+            className='btn'
+            onClick={() => {
+              navigate("/cart");
+              
+            }}
+          >
+            View Cart
+          </button>
+             {handleClick()}
+        </div>
+      </div>,
+      {
+        duration: 3500,
+        onClose: () => setClicked(false)
+      }
+    );
   }
+
+  
+
+    setClicked(true); 
+  };
+
+
+
+
   const isIncart = cartItems.some(i => i.id === New.id);
 
   return (
@@ -70,6 +95,7 @@ const playSound = () => {
   <div className="icons">
   <span className='btn_cart' onClick={()=>{
    HandleAddtoCart()
+   
   }}><FaCartArrowDown /></span>
  <span> <CiHeart /></span>
   <span><FaShare /></span>
--- src/pages/Productdetails.jsx
+++ src/pages/Productdetails.jsx
@@ -1,134 +1,197 @@
-import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router'
+import  { useContext, useEffect, useState } from "react";
+import { useParams } from "react-router";
 import { FaRegStarHalfStroke } from "react-icons/fa6";
 import { FaStar } from "react-icons/fa6";
-import './productdetails.css'
+import "./productdetails.css";
 import { TiShoppingCart } from "react-icons/ti";
 import { CiHeart } from "react-icons/ci";
 import { FaShare } from "react-icons/fa";
-import Slideproduct from '../components/slideproducts/Slideproduct';
+import Slideproduct from "../components/slideproducts/Slideproduct";
+import { CardContext } from "../components/context/Cardcontext";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router";
+
 function Productdetails() {
-    const {id} =useParams()
-    console.log(id);
-    const[product , setProduct]= useState(null)
-    const[looading, setLoading]=useState(true)
-    const[relating, setRelating]=useState([])
-    const[lloading, setLloading]=useState(true)
+  const naavigate = useNavigate();
+  const { addToCart } = useContext(CardContext);
+  const { id } = useParams();
+
+  const [product, setProduct] = useState(null);
+  const [looading, setLoading] = useState(true);
+  const [relating, setRelating] = useState([]);
+  const [lloading, setLloading] = useState(true);
+  const [check, setCheck] = useState(false);
+
     
-useEffect(()=>{
-    const fetchProduct=async()=>{
-        try{
-            const res =await fetch(`https://dummyjson.com/products/${id}`)
-            const data = await res.json()
-            setProduct(data)
-            setLoading(false)
-        }catch(error)
-        {console.error('error fetching',error)}
-        
-    }
-    fetchProduct()
-},[id])
+  const [clickedProductId, setClickedProductId] = useState(null);
 
-
-useEffect(() => {
-  if (!product) return;
-
-  const controller = new AbortController();
-  const signal = controller.signal;
-
-  fetch(`https://dummyjson.com/products/category/${product.category}`, { signal })
-    .then((res) => res.json())
-    .then((data) => {
-      setRelating(data.products);
-    })
-    .catch((error) => {
-      if (error.name === 'AbortError') {
-        console.log('Fetch aborted');
-      } else {
-        console.error('error fetching', error);
-      }
-    })
-    .finally(() => {
-      setLloading(false);
-    });
-
-  return () => {
-    controller.abort();
+  const playSound = () => {
+    const audio = new Audio("/notification.mp3");
+    audio.play();
   };
-}, [product?.category]);
 
+  const handleClick = () => {
+    playSound(); 
+  };
 
-if(looading) return <p>Loading... </p>
-if(!product) return <p>Product Not found </p>
+  const HandleAddtoCart = () => {
+    addToCart(product);
 
+   if (clickedProductId === product.id) {
+      return null ;
+    } else {
+      toast.success(
+        <div className="toastcontent">
+          <div className="toastimg">
+            <img src={product.images[0]} />
+          </div>
+          <div className="toastdetails">
+            <h3>{product.title}</h3>
+            <h5>Added To Cart</h5>
+            <button
+              className="btn"
+              onClick={() => {
+                naavigate("/cart")
+              }}
+            >
+              View Cart
+            </button>
+            {handleClick()}
+          </div>
+        </div>,
+        {
+          duration: 3500,
+          
+        }
+      )
+    }
 
+    setClickedProductId(product.id)
+    
+  };
 
+  useEffect(() => {
+    const fetchProduct = async () => {
+      try {
+        const res = await fetch(`https://dummyjson.com/products/${id}`);
+        const data = await res.json();
+        setProduct(data);
+        setLoading(false);
+      } catch (error) {
+        console.error("error fetching", error);
+      }
+    };
+    fetchProduct();
+  }, [id]);
 
+  useEffect(() => {
+    if (!product) return;
 
+    const controller = new AbortController();
+    const signal = controller.signal;
+
+    fetch(`https://dummyjson.com/products/category/${product.category}`, {
+      signal,
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setRelating(data.products);
+      })
+      .catch((error) => {
+        if (error.name === "AbortError") {
+          console.log("Fetch aborted");
+        } else {
+          console.error("error fetching", error);
+        }
+      })
+      .finally(() => {
+        setLloading(false);
+      });
+
+    return () => {
+      controller.abort();
+    };
+  }, [product?.category]);
+
+  if (looading) return <p>Loading... </p>;
+  if (!product) return <p>Product Not found </p>;
 
   return (
-
     <div>
-      <div className='itemdetails'>
-     <div className="container">
-           <div className="imagesitem">
-        <div className="bigimage">
-        <img  id="big_img" src={product.images[0]} alt={product.title} />
-        </div>
-        <div className="smalimage">
-           {product.images.map((t , index)=>{
-            return <img src={t} key={index} alt={product.title} onClick={()=>{
-              document.getElementById("big_img").src=t 
-            }}></img>
-           })}
-        </div>
+      <div className="itemdetails">
+        <div className="container">
+          <div className="imagesitem">
+            <div className="bigimage">
+              <img id="big_img" src={product.images[0]} alt={product.title} />
             </div>
+            <div className="smalimage">
+              {product.images.map((t, index) => {
+                return (
+                  <img
+                    src={t}
+                    key={index}
+                    alt={product.title}
+                    onClick={() => {
+                      document.getElementById("big_img").src = t;
+                    }}
+                  ></img>
+                );
+              })}
+            </div>
+          </div>
 
+          <div className="detailsitem">
+            <h1 className="name">{product.title}</h1>
+            <div className="stars">
+              <FaStar />
+              <FaStar />
+              <FaStar />
+              <FaStar />
+              <FaRegStarHalfStroke />
+            </div>
+            <p className="price">{product.price}$</p>
+            <h5>
+              {" "}
+              Availability : <span>{product.availabilityStatus}</span>{" "}
+            </h5>
+            <h5>
+              {" "}
+              Brand : <span>{product.brand}</span>{" "}
+            </h5>
+            <p className="para">{product.description}</p>
+            <h5 className="stock">
+              {" "}
+              <span>
+                {" "}
+                Hurry Up ! Only {product.stock} Products left in stock .{" "}
+              </span>
+            </h5>
 
-
-         <div className="detailsitem">
-              <h1 className='name'>{product.title}</h1>
-              <div className="stars">
-                  <FaStar />
-                  <FaStar />
-                  <FaStar />
-                  <FaStar />
-                  <FaRegStarHalfStroke />
-                  </div>
-               <p className='price'>{product.price}$</p>
-               <h5> Availability :  <span>{product.availabilityStatus}</span> </h5>
-               <h5> Brand :  <span>{product.brand}</span> </h5>
-              <p className='para'>{product.description}</p>
-               <h5 className='stock'> <span> Hurry Up ! Only {product.stock}  Products left in stock . </span></h5>
-
-              <button className='btn'>Add to cart <TiShoppingCart /></button>
-              <div className='icon'>
-    
-    
-    
-                  <CiHeart />
-                    <FaShare />
-               </div>
+            <button
+              className={`btn ${clickedProductId === product.id ? "incart" : "" }`}
+              onClick={() => {  HandleAddtoCart()}}
+               disabled={clickedProductId === product.id}>
+            {clickedProductId === product.id  ? `item In cart` : "Add to cart"}   <TiShoppingCart />
+            </button>
+            <div className="icon">
+              <CiHeart />
+              <FaShare />
+            </div>
+          </div>
         </div>
+      </div>
 
-     </div>
+      {lloading ? (
+        <p>isLoading...</p>
+      ) : (
+        <Slideproduct
+          key={product.category}
+          data={relating}
+          title={product.category.replace("-", "")}
+        />
+      )}
     </div>
-
-
-
-
-           {lloading ? (
-            <p>isLoading...</p>
-           ): (
-            <Slideproduct key={product.category} data={relating} title={product.category.replace("-","")} />
-           )}
-
-
-
-
-    </div>
-    
-  )
+  );
 }
 
-export default Productdetails
+export default Productdetails;
